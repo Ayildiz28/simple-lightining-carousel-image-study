b556498971e00a3756a290e49ad730c5f4e07cfc
Ahmet Yıldız <ayildiz.2861@gmail.com>
2023-05-25 23:36:41 +0200

    new carousel study

---
+++ force-app/main/default/lwc/deneme1/deneme1.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+
+
+import deneme from '@salesforce/resourceUrl/deneme';
+
+export default class deneme1 extends LightningElement {
+    deneme;
+    product1Url = '${deneme}/1.png';
+    product2Url = '${deneme}/2.png';
+    product3Url = '${deneme}/3.png';
+    product4Url = '${deneme}/4.png';
+    product5Url = '${deneme}/5.png';
+    product6Url = '${deneme}/6.png';
+    product7Url ='${deneme}/7.png';
+    product8Url = '${deneme}/8.png';
+    product9Url ='${deneme}/9.png';
+    product10Url ='${deneme}/10.png';
+
+
+}
